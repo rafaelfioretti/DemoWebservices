397b5d1d6287464c7f1d25257ef4253095a6334b
rafaelfioretti <rafaelfioretti@hotmail.com>
2016-12-17 12:28:32 -0200

    Initial commit

---
+++ app/src/main/java/br/com/rafaelfioretti/demowebservices/LoginActivity.java
@@ -0,0 +1,210 @@
+package br.com.rafaelfioretti.demowebservices;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.PersistableBundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Iterator;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+
+
+    @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_login);
+            ButterKnife.bind(this);
+            //verifyStoragePermissions(this);
+        }
+
+    @BindView(R.id.edtUsuario2)
+    EditText edtUsuario;
+
+
+    @BindView(R.id.edtSenha)
+    EditText edtSenha;
+
+
+    @OnClick(R.id.btnConectar)
+    public void logar() {
+
+        if (isConnected()){
+            new LoginTask().execute("http://10.0.2.2:3000/login");
+
+        }else{
+            Toast.makeText(this, "Você não está conectado", Toast.LENGTH_LONG);
+        }
+
+    }
+
+    public boolean isConnected() {
+        ConnectivityManager connManager = (ConnectivityManager) getSystemService(Activity.CONNECTIVITY_SERVICE);
+
+        NetworkInfo info = connManager.getActiveNetworkInfo();
+        if (info != null && info.isConnected()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
+    private class LoginTask extends AsyncTask<String, Void, String> {
+
+        String nomeusuario;
+        String senha;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            nomeusuario = edtUsuario.getText().toString();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String result = "";
+            try{
+                URL url = new URL(strings[0]);
+
+                JSONObject usuario = new JSONObject();
+                usuario.put("usuario", nomeusuario);
+
+                HttpURLConnection conn = (HttpURLConnection)
+                        url.openConnection();
+
+                conn.setReadTimeout(15000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(getPostDataString(usuario));
+
+                writer.flush();
+                writer.close();
+                os.close();
+
+                int responseCode=conn.getResponseCode();
+
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+
+                    BufferedReader in=new BufferedReader(new
+                            InputStreamReader(
+                            conn.getInputStream()));
+
+                    StringBuffer sb = new StringBuffer("");
+                    String line="";
+
+                    while((line = in.readLine()) != null) {
+
+                        sb.append(line);
+                        break;
+                    }
+
+                    in.close();
+                    result = sb.toString();
+
+                }
+                else {
+                    result =  new String("false : "+responseCode);
+                }
+            } catch (MalformedURLException e) {
+                result = e.getMessage();
+            } catch(JSONException je) {
+                result = je.getMessage();
+            } catch(IOException ioe) {
+                result = ioe.getMessage();
+            } catch (Exception e) {
+                result = e.getMessage();
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Toast.makeText(LoginActivity.this, s, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public String getPostDataString(JSONObject params) throws Exception {
+
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+
+        Iterator<String> itr = params.keys();
+
+        while(itr.hasNext()){
+
+            String key= itr.next();
+            Object value = params.get(key);
+
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(key, "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(value.toString(), "UTF-8"));
+
+        }
+        return result.toString();
+    }
+
+
+}
+
+
